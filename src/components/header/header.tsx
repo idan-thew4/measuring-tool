42331e0d921925595f179b419ec4969600a7afc3
idan-thew4 <154342235+idan-thew4@users.noreply.github.com>
2025-09-20 16:33:34 +0300

    header - wip

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,26 @@
+"use client";
+
 import header from "./header.module.scss";
+import Image from "next/image";
+import { useStore } from "../../contexts/Store";
 
 export function Header() {
-  return <header className={header.headerContainer}>Header</header>;
+  const { structure } = useStore();
+
+  return (
+    <header className={header.headerContainer}>
+      <Image
+        alt="Slil logo"
+        src="/logo.svg"
+        width={219}
+        height={60}
+        className="logo"
+      />
+      <nav>
+        {/* {structure?.header["about-section"].map((link, index) => (
+          <div key={index}>{link}</div>
+        ))} */}
+      </nav>
+    </header>
+  );
 }
