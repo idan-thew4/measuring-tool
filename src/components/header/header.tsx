c1659441e1401a9ff26de77f45db253e39931344
idan-thew4 <154342235+idan-thew4@users.noreply.github.com>
2025-09-21 16:30:52 +0300

    add clear button

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,9 +3,11 @@
 import styles from "./header.module.scss";
 import Image from "next/image";
 import { useStore } from "../../contexts/Store";
+import { useRouter } from "next/navigation";
 
 export function Header() {
   const { structure } = useStore();
+  const router = useRouter();
 
   return (
     <header className={styles["header-container"]}>
@@ -16,11 +18,23 @@ export function Header() {
         height={60}
         className={styles["logo"]}
       />
-      <nav>
-        {/* {structure?.header["about-section"].map((link, index) => (
+      {/* TO DO: remove "clear data" button */}
+
+      <button
+        onClick={() => {
+          document.cookie = `office@tdfmail.com=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+          router.push("/tool/self-assessment");
+        }}
+        className="basic-button outline negative">
+        התחל שאלון חדש
+      </button>
+      {/* TO DO: unmark <nav> */}
+
+      {/* <nav> */}
+      {/* {structure?.header["about-section"].map((link, index) => (
           <div key={index}>{link}</div>
         ))} */}
-      </nav>
+      {/* </nav> */}
     </header>
   );
 }
